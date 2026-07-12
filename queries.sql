7296137291e4f3834efbfdb98373d5b4a966cb50
Zshir <40488237+Zshir@users.noreply.github.com>
2026-07-12 04:25:58 +0300

    Add SQL query for customer and policy information
    Added a new SQL query to retrieve customer names and policy details.

--- queries.sql
+++ queries.sql
@@ -4,3 +4,12 @@ SELECT * FROM customers WHERE age > 30;
 --SQL2
 SELECT COUNT(*) FROM claims;
 
+--SQL3
+SELECT 
+    c.first_name, 
+    c.last_name, 
+    p.policy_number, 
+    p.annual_premium
+FROM customers c
+JOIN policies p ON c.customer_id = p.customer_id;
+
